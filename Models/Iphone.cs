a1e7a13f090396b195b3912783630b859392cc28
Denis <denis_hara@hotmail.com>
2023-01-21 09:20:53 -0300

    ver

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,13 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+    
+        public override void InstalarAplicativo(string nomeApp);
+        {
+           Console.WriteLine("Instalando whatsapp");
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
     }
 }
