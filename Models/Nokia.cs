dbaa2d8382a3b47df1e55912ea8727ffb13c457c
Denis <denis_hara@hotmail.com>
2023-01-23 09:20:05 -0300

    +/- numero:

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -4,9 +4,8 @@ namespace DesafioPOO.Models
     public class Nokia : Smartphone
     {
 
-        public Nokia(string numero, string modelo, string imei, int memoria) : base(modelo, numero, imei, memoria)
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         {
-            
         }
 
     public override void InstalarAplicativo(string nomeApp)
